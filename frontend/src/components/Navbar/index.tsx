2130ea9b2ee276acb6bb43d2c3987fb91988174b
Sergio Botelho <sergiorbotelho@gmail.com>
2022-01-11 23:48:57 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as GithubIcon } from 'assets/img/Github.svg'
+import './styles.css'
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/sergiorbotelho/dsmovie">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-cotact-link">/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
